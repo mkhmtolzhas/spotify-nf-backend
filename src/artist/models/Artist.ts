fd00235c11d72b59247c1c8ab01be9f628e42935
Olzhas Makhmet <mkhmtcore@gmail.com>
2024-06-17 16:59:21 +0500

    changed some things

--- src/artist/models/Artist.ts
+++ src/artist/models/Artist.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Artist = new mongoose.Schema({
     name : {type: String, required: true},
     image : {type: String, required: true},
-    description : {type: String, required: true},
+    description : {type: String, default: "20M streams"},
 })
 
 
